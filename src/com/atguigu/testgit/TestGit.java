3e4a02c22e267a5d567bd9a9bba3ca797138f468
liyb <lxf_0313@foxmail.com>
2019-04-23 20:39:05 +0800

    TestGit解决冲突

--- src/com/atguigu/testgit/TestGit.java
+++ src/com/atguigu/testgit/TestGit.java
@@ -5,11 +5,8 @@ public class TestGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git...");
-<<<<<<< HEAD
 		System.out.println("Hello TestGit2 ...");
-=======
 		System.out.println("Hello TestGit ...");
->>>>>>> branch 'master' of https://github.com/liyubao/TestGit.git
 	}
 
 }
